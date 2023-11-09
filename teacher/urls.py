cedc5e6429d870f03122c8b83ca8d252e6412918
eub <shiblu.eub@gmail.com>
2023-11-09 18:56:56 +0600

    teacher login

--- teacher/urls.py
+++ teacher/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import create_department,teacher_gives_mark,give_mark
+from .views import create_department,teacher_gives_mark,give_mark,teacher_login,teacher_deshboard
 urlpatterns = [
+    path('teacher_login',teacher_login,name='teacher-login'),
+    path('teacher_deshboard',teacher_deshboard,name='teacher-deshboard'),
+
+
     path('createdepertment',create_department,name='create-depertment'),
     path('teachergivesmark',teacher_gives_mark,name='teacher-gives-mark'),
     path('givemark/<int:subject_id>',give_mark,name='give-mark'),
