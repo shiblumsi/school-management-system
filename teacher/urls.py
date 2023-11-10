3710878652bf2bdcaa5a8510fc05802ae7bd5fef
shiblu <shiblu.msi@gmail.com>
2023-11-11 01:57:38 +0600

    Mark related functionaties Teachers deshboard

--- teacher/urls.py
+++ teacher/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import create_department,teacher_gives_mark,give_mark,teacher_login,teacher_deshboard
+from .views import create_department,teacher_gives_mark,give_mark,teacher_login,teacher_deshboard,edit_mark, view_mark
 urlpatterns = [
     path('teacher_login',teacher_login,name='teacher-login'),
     path('teacher_deshboard',teacher_deshboard,name='teacher-deshboard'),
@@ -7,4 +7,8 @@ urlpatterns = [
     path('createdepertment',create_department,name='create-depertment'),
     path('teachergivesmark',teacher_gives_mark,name='teacher-gives-mark'),
     path('givemark/<int:subject_id>',give_mark,name='give-mark'),
+    path('viewmark/<int:subject_id>',view_mark,name='view-mark'),
+    path('editmark/<int:mark_id>',edit_mark,name='edit-mark'),
+    
+    #path('get_height_mark',get_height_mark,name='get-height-mark'),
 ]
